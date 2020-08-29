b5cab124ee7f830b6a6483239e58685e1d23542e
Matheus Rodrigues dos Santos <matheus.rodrisantos@bol.com.br>
2020-08-28 22:46:02 -0300

    palavra reservada final

--- final/final.php
+++ final/final.php
@@ -1,4 +1,8 @@
 <?php 
+/* A palavra reservada Final pode ser usada tanto para classes quanto para metodos
+no uso da classe ela não permite que alguma outra classe tente estender dela; e em metodos ela não permite 
+que  classes filhas venham o metodo com o mesmo nome
+*/
 
 final class User
 {
